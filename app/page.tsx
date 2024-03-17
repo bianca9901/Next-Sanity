afa6c1a86bee838c393e6509e36826311bb6bfbb
bianca9901 <biancawikberg99@yahoo.com>
2024-03-17 09:26:59 +0100

    Remove styles that are now in Layout instead

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const projects = await getProjects();
 
   return (
-    <div className="max-w-5xl mx-auto py-20">
+    <div>
       <h1 className="text-7xl font-extrabold">
         Hello, I&apos;m{" "}
         <span
